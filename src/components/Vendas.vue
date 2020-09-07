7d3afe9e689d2a80bf40fb3e1f08f3ba9ff061c7
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2020-09-06 21:35:24 -0300

    EB:FIN: Correções responsividade

--- src/components/Vendas.vue
+++ src/components/Vendas.vue
@@ -1,29 +1,51 @@
 <template>
   <div class="container">
-    <h5 class="center">{{titulo}}</h5>
+    <h5 class="center">{{ titulo }}</h5>
     <div class="content">
       <div class="row">
-        <div class="input-field col s3">
-          <input id="data_caixa" type="text" class="validate" v-model="venda.data" />
+        <div class="input-field col s12 m6 l3">
+          <input
+            id="data_caixa"
+            type="text"
+            class="validate"
+            v-model="venda.data"
+          />
           <label for="data_caixa">Data</label>
         </div>
-        <div class="input-field col s3">
-          <input id="valor_inverno" type="text" class="validate" v-model="venda.valorInverno" />
+        <div class="input-field col s12 m6 l3">
+          <input
+            id="valor_inverno"
+            type="text"
+            class="validate"
+            v-model="venda.valorInverno"
+          />
           <label for="valor_inverno">Valor venda inverno</label>
         </div>
-        <div class="input-field col s3">
-          <input id="valor_verao" type="text" class="validate" v-model="venda.valorVerao" />
+        <div class="input-field col s12 m6 l3">
+          <input
+            id="valor_verao"
+            type="text"
+            class="validate"
+            v-model="venda.valorVerao"
+          />
           <label for="valor_verao">Valor venda verao</label>
         </div>
-        <div class="input-field col s3">
-          <input id="valor_caixa" type="text" class="validate" v-model="venda.valorTotal" />
+        <div class="input-field col s12 m6 l3">
+          <input
+            id="valor_caixa"
+            type="text"
+            class="validate"
+            v-model="venda.valorTotal"
+          />
           <label for="valor_caixa">Valor Total</label>
         </div>
       </div>
       <div class="right-align col s12">
         <p>
           <a class="btn-flat" @click="limparVenda()">Limpar</a>
-          <a @click="adicionarVenda()" class="btn waves-effect waves-light">Confirmar</a>
+          <a @click="adicionarVenda()" class="btn waves-effect waves-light"
+            >Confirmar</a
+          >
         </p>
       </div>
     </div>
@@ -51,7 +73,7 @@
 
     <div class="container">
       <div class="row center">
-        <div class="col s6">
+        <div class="col s12 m6 l3">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
               <span class="card-title">Valor total inverno</span>
@@ -60,7 +82,7 @@
           </div>
         </div>
 
-        <div class="col s6">
+        <div class="col s12 m6 l3">
           <div class="card grey darken-3">
             <div class="card-content white-text">
               <span class="card-title">Valor total verao</span>
@@ -69,7 +91,7 @@
           </div>
         </div>
 
-        <div class="col s6">
+        <div class="col s12 m6 l3">
           <div class="card blue-grey darken-4">
             <div class="card-content white-text">
               <span class="card-title">Comissao inverno</span>
@@ -78,7 +100,7 @@
           </div>
         </div>
 
-        <div class="col s6">
+        <div class="col s12 m6 l3">
           <div class="card blue-grey lighten-2">
             <div class="card-content white-text">
               <span class="card-title">Comissao verao</span>
@@ -101,106 +123,111 @@
 </template>
 
 <script>
-import axios from "axios";
-import Materialize from "materialize-css";
+  import axios from "axios";
+  import Materialize from "materialize-css";
 
-export default {
-  name: "Vendas",
-  data() {
-    return {
-      titulo: "Lançamento de vendas",
-      listaVenda: [],
-      vendasSomadas: null,
-      vendasSomadasInverno: null,
-      vendasSomadasVerao: null,
-      comissaoVerao: null,
-      comissaoInverno: null,
-      venda: {
-        id: Materialize.guid(),
-        data: null,
-        valorTotal: null,
-        valorInverno: null,
-        valorVerao: null,
-      },
-    };
-  },
-  mounted() {
-    this.atualizarValores();
-  },
-  computed: {
-    valorTotal() {
-      return this.venda.valorInverno + this.venda.valorVerao;
-    },
-  },
-
-  methods: {
-    limparVenda() {
-      this.venda.data = null;
-      this.venda.valorTotal = null;
-      this.venda.valorInverno = null;
-      this.venda.valorVerao = null;
-    },
-    adicionarVenda() {
-      var venda = {
-        data: this.venda.data,
-        valor: parseInt(this.venda.valorTotal),
-        valorInverno: parseInt(this.venda.valorInverno),
-        valorVerao: parseInt(this.venda.valorVerao),
+  export default {
+    name: "Vendas",
+    data() {
+      return {
+        titulo: "Lançamento de vendas",
+        listaVenda: [],
+        vendasSomadas: null,
+        vendasSomadasInverno: null,
+        vendasSomadasVerao: null,
+        comissaoVerao: null,
+        comissaoInverno: null,
+        venda: {
+          id: Materialize.guid(),
+          data: null,
+          valorTotal: null,
+          valorInverno: null,
+          valorVerao: null,
+        },
       };
-      axios.post(`http://localhost:3000/vendas/`, venda).then((resposta) => {
-        console.log("Produto adicionado com sucesso" + resposta);
-      });
-
-      this.listaVenda.push(venda);
-      this.limparVenda();
     },
-    removerVenda() {},
-    atualizarValores() {
-      axios
-        .get("https://my-json-server.typicode.com/KicKerBNU/fin/vendas")
-        .then((resposta) => {
-          this.listaVenda = resposta.data;
-          console.log(resposta);
-
-          this.vendasSomadas = this.listaVenda.reduce(vendasSomadas, 0);
-          function vendasSomadas(total, item) {
-            return total + item.valor;
-          }
-
-          this.vendasSomadasInverno = this.listaVenda.reduce(
-            vendasSomadasInverno,
-            0
-          );
-          function vendasSomadasInverno(total, item) {
-            return total + item.valorInverno;
-          }
-
-          this.vendasSomadasVerao = this.listaVenda.reduce(
-            vendasSomadasVerao,
-            0
-          );
-          function vendasSomadasVerao(total, item) {
-            return total + item.valorVerao;
-          }
-
-          this.comissaoVerao = this.vendasSomadasVerao * 0.13;
-          this.comissaoInverno = this.vendasSomadasInverno * 0.13;
-        });
+    mounted() {
+      this.atualizarValores();
     },
-  },
-};
+    computed: {
+      valorTotal() {
+        return this.venda.valorInverno + this.venda.valorVerao;
+      },
+    },
+
+    methods: {
+      limparVenda() {
+        this.venda.data = null;
+        this.venda.valorTotal = null;
+        this.venda.valorInverno = null;
+        this.venda.valorVerao = null;
+      },
+      adicionarVenda() {
+        var venda = {
+          data: this.venda.data,
+          valor: parseInt(this.venda.valorTotal),
+          valorInverno: parseInt(this.venda.valorInverno),
+          valorVerao: parseInt(this.venda.valorVerao),
+        };
+        axios
+          .post(
+            `https://my-json-server.typicode.com/KicKerBNU/fin/vendas/`,
+            venda
+          )
+          .then((resposta) => {
+            console.log("Produto adicionado com sucesso" + resposta);
+          });
+
+        this.listaVenda.push(venda);
+        this.limparVenda();
+      },
+      removerVenda() {},
+      atualizarValores() {
+        axios
+          .get("https://my-json-server.typicode.com/KicKerBNU/fin/vendas")
+          .then((resposta) => {
+            this.listaVenda = resposta.data;
+            console.log(resposta);
+
+            this.vendasSomadas = this.listaVenda.reduce(vendasSomadas, 0);
+            function vendasSomadas(total, item) {
+              return total + item.valor;
+            }
+
+            this.vendasSomadasInverno = this.listaVenda.reduce(
+              vendasSomadasInverno,
+              0
+            );
+            function vendasSomadasInverno(total, item) {
+              return total + item.valorInverno;
+            }
+
+            this.vendasSomadasVerao = this.listaVenda.reduce(
+              vendasSomadasVerao,
+              0
+            );
+            function vendasSomadasVerao(total, item) {
+              return total + item.valorVerao;
+            }
+
+            this.comissaoVerao = this.vendasSomadasVerao * 0.13;
+            this.comissaoInverno = this.vendasSomadasInverno * 0.13;
+          });
+      },
+    },
+  };
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 </style>
